c87aa5ec9134b346a3597115c80c14902ef5d199
sks-syber <100261628+sks-syber@users.noreply.github.com>
2024-07-27 17:04:27 +0530

    adding floating point number

--- sum.py
+++ sum.py
@@ -1,5 +1,5 @@
-num1=int(input("Enter your first number"))
-num2=int(input("Enter your second number"))
+num1=float(input("Enter your first number"))
+num2=float(input("Enter your second number"))
 
 result=num1+num2
 print("Sum of your two number is ",result)
