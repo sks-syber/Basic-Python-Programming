19288174eabbf43ae1dfb8eec420a7bc1958c8d5
sks-syber <100261628+sks-syber@users.noreply.github.com>
2024-07-27 16:57:22 +0530

    odd or even

---
+++ odd_or_even.py
@@ -0,0 +1,8 @@
+# Write a program to print the given number is odd or even.
+number =int(input("Enter your number: "))
+
+if number%2==0:
+    print("Entered Number is Even Number")
+else:
+    print("The number is Odd Number")
+
