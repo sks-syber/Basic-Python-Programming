bbe53bdd60388b1e8f5b7b6913d6dda66272fdfc
sks-syber <100261628+sks-syber@users.noreply.github.com>
2024-07-28 09:32:31 +0530

    entered number is negative or positive or zero

---
+++ positive_or_negative_number.py
@@ -0,0 +1,10 @@
+number=int(input("Enter your number"))
+
+if number>0:
+    print("The number is positive")
+
+elif number==0:
+    print("The number is zero")
+
+else:
+    print("The number is negative number")
